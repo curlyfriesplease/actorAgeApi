6242029fad88937bda789d2ed3208357bd8570f9
curlyfriesplease <jmulhern85+github@gmail.com>
2024-06-09 11:55:48 +0100

    Adds birthdays to specific actors test

--- tests/getTwoSpecificActors.test.ts
+++ tests/getTwoSpecificActors.test.ts
@@ -5,12 +5,16 @@ describe('getTwoSpecificActors', () => {
     const testActors = await getTwoSpecificActors(31, 2039);
     expect(testActors).toHaveLength(2);
 
-    expect(testActors[0]).toHaveProperty('birthday');
     expect(testActors[0].name).toEqual('Tom Hanks');
+    expect(testActors[0]).toHaveProperty('birthday');
+    expect(testActors[0]).toHaveProperty('known_for');
+    expect(testActors[0].birthday).toEqual('1956-07-09');
     expect(testActors[0].known_for).toHaveLength(3);
 
-    expect(testActors[1]).toHaveProperty('birthday');
     expect(testActors[1].name).toEqual('Brendan Gleeson');
+    expect(testActors[1]).toHaveProperty('birthday');
+    expect(testActors[1]).toHaveProperty('known_for');
+    expect(testActors[1].birthday).toEqual('1955-03-29');
     expect(testActors[1].known_for).toHaveLength(3);
   });
 });
